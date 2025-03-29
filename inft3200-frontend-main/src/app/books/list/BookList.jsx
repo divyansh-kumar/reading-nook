c5c9466a826f982c6bc288b508c49b301c60b36c
Divyansh <kumardivyansh59@gmail.com>
2025-03-29 19:24:34 -0400

    test

--- inft3200-frontend-main/src/app/books/list/BookList.jsx
+++ inft3200-frontend-main/src/app/books/list/BookList.jsx
@@ -17,7 +17,7 @@ export default function BookListPage() {
   const fetchBooks = async () => {
     try {
       const response = await fetch(
-        `http://127.0.0.1:8080/books`,
+        `http://${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}/books`,
         { cache: 'no-store' }
       );
       if (!response.ok) {
